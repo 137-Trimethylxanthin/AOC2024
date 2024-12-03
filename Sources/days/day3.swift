3579bbc58529356f88ab8a4c7fd4e15b715c8b45
137-Trimethylxanthin <xxv.many@gmail.com>
2024-12-03 15:23:11 +0100

    Happy Third Day

--- Sources/days/day3.swift
+++ Sources/days/day3.swift
@@ -31,53 +31,49 @@ struct Day3 {
 
 
     // Solve Part 2 of the Day 2 challenge
-    func solvePart2() {
-        let preFormat = "do()" + FileTools.readFile(from: inputPath) + "don't()"
-
+func solvePart2() {
+    let text = FileTools.readFile(from: inputPath)
+    var isMulEnabled = true
+    var result = 0
     
+    let regex = try? NSRegularExpression(
+        pattern: #"(do\(\)|don't\(\)|mul\((\d+),(\d+)\))"#,
+        options: []
+    )
+    
+    let matches = regex?.matches(
+        in: text,
+        range: NSRange(location: 0, length: text.utf16.count)
+    )
+    
+    matches?.forEach { match in
+        let fullMatchRange = match.range(at: 0)
+        let fullMatch = (text as NSString).substring(with: fullMatchRange)
         
-        let format = try? NSRegularExpression(
-            pattern: #"do\(\).*?don't\(\)"#,
-            options: [.anchorsMatchLines] // Equivalent to multiline 'm' flag
-        )
-
-        let formatMatches = format?.matches(
-            in: preFormat,
-            range: NSRange(location: 0, length: preFormat.utf16.count)
-        )
-        var text = ""
-        var matchedTexts: [String] = []
-            
-            // Process all matches for the first regex
-            formatMatches?.forEach { match in
-                // Extract the substring from the range of the match and store it in the array
-                let matchRange = match.range
-                let matchedText = (preFormat as NSString).substring(with: matchRange)
-                matchedTexts.append(matchedText)
+        if fullMatch == "do()" {
+            isMulEnabled = true
+        } else if fullMatch == "don't()" {
+            isMulEnabled = false
+        } else if fullMatch.range(of: #"mul\((\d+),(\d+)\)"#, options: .regularExpression) != nil {
+            if isMulEnabled {
+                // Use regex to extract numbers
+                let numberRegex = try? NSRegularExpression(pattern: #"(\d+)"#)
+                let numberMatches = numberRegex?.matches(in: fullMatch, range: NSRange(location: 0, length: fullMatch.utf16.count))
+                
+                if numberMatches?.count == 2 {
+                    let firstNumberRange = numberMatches?[0].range(at: 1)
+                    let secondNumberRange = numberMatches?[1].range(at: 1)
+                    
+                    let firstNumber = Int((fullMatch as NSString).substring(with: firstNumberRange!))!
+                    let secondNumber = Int((fullMatch as NSString).substring(with: secondNumberRange!))!
+                    
+                    result += firstNumber * secondNumber
+                }
             }
-
-        text = matchedTexts.joined()
-
-        let regex: NSRegularExpression? = try? NSRegularExpression(
-            pattern: #"mul\((\d+),(\d+)\)"#,
-                        options: [.anchorsMatchLines] // Equivalent to multiline 'm' flag
-        )
-        let matches = regex?.matches(
-            in: text,
-            range: NSRange(location: 0, length: text.utf16.count)
-        )
-        var result = 0
-        matches?.forEach {match in
-            let firstNumberRange = match.range(at: 1)
-            let secondNumberRange = match.range(at: 2)
-            
-            let firstNumber = Int((text as NSString).substring(with: firstNumberRange))!
-            let secondNumber = Int((text as NSString).substring(with: secondNumberRange))!
-            
-            result += firstNumber * secondNumber
         }
-
-        print("Part 2 Solution: \(result)")
     }
+    
+    print("Part 2 Solution: \(result)")
+}
 
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -11,9 +11,9 @@ case 2:
     day2Solution.solvePart2()
 case 3:
     let day3 = Day3()
-    print("Day 2 Part 1 Solution:")
+    print("Day 3 Part 1 Solution:")
     day3.solvePart1()
-    print("\nDay 2 Part 2 Solution:")
+    print("\nDay 3 Part 2 Solution:")
     day3.solvePart2()
 default:
     print("Solution for Day \(day) not implemented")
